c74d35bd21d4d72e41a8cf8634a1bb389bdea3dc
Hugo Aguirre <bul.ikana@gmail.com>
2019-06-15 20:42:10 -0500

    Docs for github pages

---
+++ docs/offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1560648603,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/hummingbird-sheet0.png",
+		"images/hummingbird-sheet1.png",
+		"images/redflower-sheet0.png",
+		"images/blueflower-sheet0.png",
+		"images/yellowflower-sheet0.png",
+		"images/plant-sheet0.png",
+		"images/redparticles.png",
+		"images/redorb-sheet0.png",
+		"images/yelloworb-sheet0.png",
+		"images/yellowparticles.png",
+		"images/blueorb-sheet0.png",
+		"images/blueparticles.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
---
+++ readme.md
@@ -0,0 +1,11 @@
+# ![](Animations/hummingbird/Deafult/000.png) Pollumination
+
+## Lights, colors, and hummingbirds
+
+![](docs/screenshot.png)
+
+### Eternal Work In Progress
+
+Pollumination was intended to be a game where hummingbirds collected light particles to pollinate flowers.
+
+In their current state, Pullomination was just a practice of pixel art drawing and animations, and particle emitters in Construct2.
